74840446854e00c6191354e136cea455cb4c5cd0
greentfrapp <limsweekiat@gmail.com>
2024-12-11 16:23:53 +0800

    Add room and messaging

--- src/index.py
+++ src/index.py
@@ -8,15 +8,24 @@ socketio_app = socketio.ASGIApp(sio, app)
 
 # app.mount("/", socket_app)
 
+room_id = "123"
+
 
 @sio.event
 async def connect(sid, env):
+    await sio.enter_room(sid, room_id)
     print("New Client Connected to This id :" + " " + str(sid))
 
 
 @sio.event
 async def disconnect(sid):
+    await sio.leave_room(sid, room_id)
     print("Client Disconnected: " + " " + str(sid))
+
+
+@sio.event
+async def message(sid, data):
+    await sio.emit("message", data, room=room_id, skip_sid=sid)
 
 
 @app.get("/")
